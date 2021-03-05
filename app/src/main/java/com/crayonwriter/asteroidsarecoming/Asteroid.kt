6196a0b46b1cfd34d21cbf72dca73822481742b7
Theda Rogers <crayonwriter@gmail.com>
2021-03-05 00:45:11 -0500

    Feature: Annotate the asteroid data class and give it a new table name

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/Asteroid.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/Asteroid.kt
@@ -1,11 +1,16 @@
 package com.crayonwriter.asteroidsarecoming
 
 import android.os.Parcelable
+import androidx.room.Entity
 import kotlinx.android.parcel.Parcelize
 
-//TODO: Annotate this class to make it a table for DAO
+//Annotate this class to make it a table for DAO and give it a new name
+
 @Parcelize
+
+//Annotate this class to make it a table for DAO and give it a new name
+@Entity(tableName = "asteroid_table")
 data class Asteroid(val id: Long = 0L, val codename: String, val closeApproachDate: String,
-                    val absoluteMagnitude: Double, val estimatedDiameter: Double,
-                    val relativeVelocity: Double, val distanceFromEarth: Double,
-                    val isPotentiallyHazardous: Boolean) : Parcelable
+                               val absoluteMagnitude: Double, val estimatedDiameter: Double,
+                               val relativeVelocity: Double, val distanceFromEarth: Double,
+                               val isPotentiallyHazardous: Boolean) : Parcelable
