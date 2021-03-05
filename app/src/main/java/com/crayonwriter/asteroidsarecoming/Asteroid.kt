07d9825cc0e953598c8bf3494625f7f9af0ab097
Theda Rogers <crayonwriter@gmail.com>
2021-03-05 00:42:26 -0500

    Create TODO and initialize id to 0L

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/Asteroid.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/Asteroid.kt
@@ -3,8 +3,9 @@ package com.crayonwriter.asteroidsarecoming
 import android.os.Parcelable
 import kotlinx.android.parcel.Parcelize
 
+//TODO: Annotate this class to make it a table for DAO
 @Parcelize
-data class Asteroid(val id: Long, val codename: String, val closeApproachDate: String,
+data class Asteroid(val id: Long = 0L, val codename: String, val closeApproachDate: String,
                     val absoluteMagnitude: Double, val estimatedDiameter: Double,
                     val relativeVelocity: Double, val distanceFromEarth: Double,
                     val isPotentiallyHazardous: Boolean) : Parcelable
