296e2995a3bd9b0b4281269a40676ed885407850
Theda Rogers <crayonwriter@gmail.com>
2021-03-07 16:32:00 -0500

    Feature: Create Util.kt file and create TextItemViewHolder class

---
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/AsteroidAdapter.kt
@@ -0,0 +1,8 @@
+package com.crayonwriter.asteroidsarecoming.main
+
+import androidx.recyclerview.widget.RecyclerView
+import com.crayonwriter.asteroidsarecoming.TextItemViewHolder
+
+class AsteroidAdapter: RecyclerView.Adapter<TextItemViewHolder>()
+
+}
