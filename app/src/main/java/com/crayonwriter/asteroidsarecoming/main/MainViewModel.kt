5f6e79dc5178038f02c1ec6f8494810adc9bbf35
Theda Rogers <crayonwriter@gmail.com>
2021-03-06 19:48:32 -0500

    Create ViewModel and ViewModelFactory files

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainViewModel.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainViewModel.kt
@@ -1,6 +1,11 @@
 package com.crayonwriter.asteroidsarecoming.main
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.ViewModel
+import com.crayonwriter.asteroidsarecoming.database.AsteroidDatabaseDao
 
-class MainViewModel : ViewModel() {
+class MainViewModel(
+    val database: AsteroidDatabaseDao,
+    application: Application) : AndroidViewModel(application) {
 }
---
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainViewModelFactory.kt
@@ -0,0 +1,17 @@
+package com.crayonwriter.asteroidsarecoming.main
+
+import android.app.Application
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.crayonwriter.asteroidsarecoming.database.AsteroidDatabaseDao
+
+class MainViewModelFactory(
+    private val dataSource: AsteroidDatabaseDao,
+    private val application: Application) : ViewModelProvider.Factory {
+        override fun <T: ViewModel?> create(modelClass: Class<T>): T {
+            if (modelClass.isAssignableFrom(MainViewModel::class.java)) {
+                return MainViewModel(dataSource, application) as T
+            }
+            throw IllegalArgumentException("Unknown ViewModel class")
+        }
+    }
