d568900839306159b608d31722e8ff66314b2456
Theda Rogers <crayonwriter@gmail.com>
2021-03-07 00:07:25 -0500

    Feature: Connect viewmodel and factory to the main fragment file and layout

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainFragment.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainFragment.kt
@@ -5,6 +5,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.crayonwriter.asteroidsarecoming.R
+import com.crayonwriter.asteroidsarecoming.database.AsteroidDatabase
 import com.crayonwriter.asteroidsarecoming.databinding.FragmentMainBinding
 
 class MainFragment : Fragment() {
@@ -16,11 +17,27 @@ class MainFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         val binding = FragmentMainBinding.inflate(inflater)
-        binding.lifecycleOwner = this
-
-        binding.viewModel = viewModel
 
         setHasOptionsMenu(true)
+
+        //Reference to the application the fragment is attached to, to pass into the viewmodelfactory provider
+        val application = requireNotNull(this.activity).application
+
+        //Reference to the data source via a reference to the dao
+        val dataSource = AsteroidDatabase.getInstance(application).asteroidDatabaseDao
+
+        //create an instance of the viewmodelfactory
+        val viewModelFactory = MainViewModelFactory(dataSource, application)
+
+        //Now that we have a factory, we can ask the viewmodelfactory provider for a mainViewModel
+        val mainViewModel =
+            ViewModelProvider(
+                this, viewModelFactory).get(MainViewModel::class.java)
+
+        //Setup databinding, including setting the variable in the layout
+        // which we access through the binding object to the viewModel
+        binding.mainViewModel = mainViewModel
+        binding.lifecycleOwner = this
 
         return binding.root
     }
