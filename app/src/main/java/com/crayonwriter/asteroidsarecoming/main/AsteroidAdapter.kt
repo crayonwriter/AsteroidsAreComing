9ffd5050beec9e298e68f55d5d88e370a2b6bc30
Theda Rogers <crayonwriter@gmail.com>
2021-03-07 23:06:54 -0500

    Feature: Finish code for the adapter, inflating fragment_main layout

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/main/AsteroidAdapter.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/AsteroidAdapter.kt
@@ -1,8 +1,32 @@
 package com.crayonwriter.asteroidsarecoming.main
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.crayonwriter.asteroidsarecoming.Asteroid
+import com.crayonwriter.asteroidsarecoming.R
 import com.crayonwriter.asteroidsarecoming.TextItemViewHolder
 
-class AsteroidAdapter: RecyclerView.Adapter<TextItemViewHolder>()
+//This class will take a list of asteroids and adapt it to something recyclerview can display
+class AsteroidAdapter: RecyclerView.Adapter<TextItemViewHolder>() {
+    var asteroidData = listOf<Asteroid>()
+        set(value) {
+         field = value
+         notifyDataSetChanged()
+        }
+    override fun getItemCount() = asteroidData.size
+
+    override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
+        val item = asteroidData[position]
+        holder.textView.text = item.codename
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
+        val layoutInflater = LayoutInflater.from(parent.context)
+        val view = layoutInflater.inflate(R.layout.fragment_main, parent, false) as TextView
+        return TextItemViewHolder(view)
+    }
+
 
 }
