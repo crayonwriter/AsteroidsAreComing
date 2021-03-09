544477c97bb8be7cd13cda29f159d58ccc02c828
Theda Rogers <crayonwriter@gmail.com>
2021-03-09 00:33:36 -0500

    Update: Use insert method with a coroutine to insert the data

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainViewModel.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/main/MainViewModel.kt
@@ -2,26 +2,40 @@ package com.crayonwriter.asteroidsarecoming.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.crayonwriter.asteroidsarecoming.Asteroid
 import com.crayonwriter.asteroidsarecoming.database.AsteroidDatabaseDao
+import kotlinx.coroutines.launch
 
 class MainViewModel(
     val database: AsteroidDatabaseDao,
     application: Application) : AndroidViewModel(application) {
 
-        val asteroids = database.getAsteroidList()
+    val asteroids = database.getAsteroidList()
 
-        init {
-            insertSampleAsteroidList()
-        }
-
-    private fun insertSampleAsteroidList() =
-        listOf(
-            Asteroid(0L, "Theda", "January 2", 2.45,
-                4.3, 5.3, 300.0, true),
-            Asteroid(1L, "Gabrielle", "June 22", 4.2,
-                3.4, 5.55, 500.0, false),
-            Asteroid(2L, "Jade", "May 20", 45.3, 400.2,
-            76.1, 1000.0, true))
+    init {
+        insertSampleAsteroidList()
     }
+
+    private fun insertSampleAsteroidList() {
+        val asteroid =
+            (
+            Asteroid(
+                0L, "Theda", "January 2", 2.45,
+                4.3, 5.3, 300.0, true
+            )
+//            Asteroid(
+//                1L, "Gabrielle", "June 22", 4.2,
+//                3.4, 5.55, 500.0, false
+//            ),
+//            Asteroid(
+//                2L, "Jade", "May 20", 45.3, 400.2,
+//                76.1, 1000.0, true
+//            )
+        )
+
+    viewModelScope.launch {
+        database.insert(asteroid)
+    }
+    }
+}
