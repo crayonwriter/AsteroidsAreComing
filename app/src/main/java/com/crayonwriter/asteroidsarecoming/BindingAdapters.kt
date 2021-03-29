05b3820c3e38b0d1cf5e9cbf1217e1c8fb105930
Theda Rogers <crayonwriter@gmail.com>
2021-03-29 11:23:14 -0400

    Feat: Add bindingadapters for date and codename

--- app/src/main/java/com/crayonwriter/asteroidsarecoming/BindingAdapters.kt
+++ app/src/main/java/com/crayonwriter/asteroidsarecoming/BindingAdapters.kt
@@ -3,6 +3,23 @@ package com.crayonwriter.asteroidsarecoming
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import org.w3c.dom.Text
+
+//Create BindingAdapter for the date and the name
+
+@BindingAdapter("closeApproachDateString")
+fun TextView.setCloseApproachDateString(item: Asteroid?) {
+    item?.let{
+        text = item.closeApproachDate
+    }
+}
+
+@BindingAdapter("codenameString")
+fun TextView.setCodenameString(item: Asteroid?) {
+    item?.let {
+        text = item.codename
+    }
+}
 
 @BindingAdapter("statusIcon")
 fun bindAsteroidStatusImage(imageView: ImageView, isHazardous: Boolean) {
@@ -23,12 +40,14 @@ fun bindDetailsStatusImage(imageView: ImageView, isHazardous: Boolean) {
 }
 
 @BindingAdapter("astronomicalUnitText")
+//absolute magnitude or miss_distance in close approach data
 fun bindTextViewToAstronomicalUnit(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.astronomical_unit_format), number)
 }
 
 @BindingAdapter("kmUnitText")
+//estimated diameter
 fun bindTextViewToKmUnit(textView: TextView, number: Double) {
     val context = textView.context
     textView.text = String.format(context.getString(R.string.km_unit_format), number)
